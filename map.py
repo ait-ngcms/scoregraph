edafae4348446986d116a041814070375ebb6afe
Roman <roman.graf@ait.ac.at>
2015-09-18 11:02:16 +0200

    + refactoring for GND - wikidata author data mapping

--- map.py
+++ map.py
@@ -3,36 +3,27 @@
 Script for summarizing data for statistics generation.
 
 Invocation:
-$ python map.py data/normalized/*.json -o summary.csv
+$ python map.py data/normalized/*.json -o summary_authors.csv
 """
 
 import argparse
 import csv
-import json
 import sys
 import codecs
 
 import requests
-
-import common
-
-import goslate
-
-from common import read_records
-
-import dbpedia_helper
 
 
 ONB_COL = 0
 NAME_COL = 1
 GND_COL = 2
 
-#HTTP = 'http://'
 
 WIKIDATA_API_URL = 'https://wdq.wmflabs.org/api?q='
 ITEMS_JSON = 'items'
 PROPS_JSON = 'props'
 VALUE_POS_IN_WIKIDATA_PROP_LIST = 2
+
 
 OCCUPATION_PROP              = 106
 VIAF_ID_PROP                 = 214
@@ -44,6 +35,7 @@ FREEBASE_ID_PROP             = 646
 GENRE_PROP                   = 136
 IMSLP_ID_PROP                = 839
 NTA_ID_PROP                  = 1006
+
 
 properties = [
     OCCUPATION_PROP
@@ -58,8 +50,26 @@ properties = [
     , NTA_ID_PROP
 ]
 
+wikidata_author_fieldnames = [
+    'gnd'
+    , 'wikidata'
+    , 'onb'
+    , 'name'
+    , 'genre'
+    , 'occupation'
+    , 'freebase'
+    , 'viaf'
+    , 'bnf'
+    , 'nkc'
+    , 'nta'
+    , 'imslp'
+    , 'dbpedia'
+    , 'music_brainz_artist_id'
+]
+
 
 def process_http_query(query):
+
     r = requests.get(query)
     if(r.status_code != 200):
         print("Request error:", r.url)
@@ -67,6 +77,7 @@ def process_http_query(query):
 
 
 def extract_property_value(response, property):
+
     values = ''
     try:
         json_data = response.json()
@@ -79,104 +90,125 @@ def extract_property_value(response, property):
     return values
 
 
+def extract_wikidata_author_id(response):
+
+    try:
+        return response.json()[ITEMS_JSON][0]
+    except IndexError as ie:
+        print 'No items found!', ie
+    return None
+
+
+def build_wikidata_author_entry(
+        author_response_json, line, wikidata_author_id):
+
+    row = line.split(";")
+    genres = extract_property_value(author_response_json, GENRE_PROP)
+    occupations = extract_property_value(author_response_json, OCCUPATION_PROP)
+    freebase = extract_property_value(author_response_json, FREEBASE_ID_PROP)
+    viaf = extract_property_value(author_response_json, VIAF_ID_PROP)
+    bnf = extract_property_value(author_response_json, BNF_ID_PROP)
+    nkc = extract_property_value(author_response_json, NKC_ID_PROP)
+    nta = extract_property_value(author_response_json, NTA_ID_PROP)
+    imslp = extract_property_value(author_response_json, IMSLP_ID_PROP)
+    music_brainz_artist = extract_property_value(author_response_json, MUSIC_BRAINZ_ARTIST_ID_PROP)
+
+    values = [
+        row[GND_COL]
+        , wikidata_author_id
+        , row[ONB_COL]
+        , row[NAME_COL]
+        , genres
+        , occupations
+        , freebase
+        , viaf
+        , bnf
+        , nkc
+        , nta
+        , imslp
+        , ''
+        , music_brainz_artist
+    ]
+
+    return dict(zip(wikidata_author_fieldnames, values))
+
+
+# query Wikidata by GND ID, where GND Identifier is a property P227
+# e.g. https://wdq.wmflabs.org/api?q=string[227:118576291] for Gustav Mahler
+def retrieve_wikidata_author_id(gnd):
+
+    query = WIKIDATA_API_URL + 'string[' + str(GND_ID_PROP) + ':' + gnd + ']'
+    print 'query:', query
+    wikidata_author_id_response = process_http_query(query)
+    print 'response content:', wikidata_author_id_response.content
+    return wikidata_author_id_response
+
+
+# query Wikidata by wikidata ID for an author
+# e.g. https://www.wikidata.org/wiki/Q7304 for Gustav Mahler in browser
+#      https://wdq.wmflabs.org/api?q=items[7304]&props=106,136 in API
+def retrieve_wikidata_author_data(wikidata_author_id):
+
+    query_author = WIKIDATA_API_URL + ITEMS_JSON + '[' + str(wikidata_author_id) + ']&' + \
+                   PROPS_JSON + '=' + ", ".join(str(e) for e in properties)
+    print 'query author:', query_author
+    author_response_json = process_http_query(query_author)
+    print 'author json data:', author_response_json
+    return author_response_json
+
+
+def extract_gnd_from_line(line):
+
+    try:
+        row = line.split(";")
+        return row[GND_COL].split('/')[-1]
+    except IndexError as ie:
+        print 'No GND found!', ie
+    return None
+
+
+def get_wikidata_author_id_by_gnd(gnd):
+
+    wikidata_author_id_response = retrieve_wikidata_author_id(gnd)
+    wikidata_author_id = extract_wikidata_author_id(wikidata_author_id_response)
+    print 'wikidata_author_id', wikidata_author_id
+    return wikidata_author_id
+
+
+def store_author_data_by_gnd(inputfile, writer):
+
+    # GND cache contains aggregated GND IDs
+    gnd_cache = []
+
+    f = codecs.open(inputfile, 'r')
+    for idx, line in enumerate(f):
+        print repr(line)
+        if idx > 0:
+            gnd = extract_gnd_from_line(line)
+            store_author_data(writer, gnd, gnd_cache, line)
+
+
+def store_author_data(writer, gnd, gnd_cache, line):
+
+    if gnd not in gnd_cache:
+        wikidata_author_id = get_wikidata_author_id_by_gnd(gnd)
+        if(wikidata_author_id and wikidata_author_id not in gnd_cache):
+            gnd_cache.append(wikidata_author_id)
+            author_response_json = retrieve_wikidata_author_data(wikidata_author_id)
+            # store author wikidata JSON in file onb_id_wikidata_id.json
+            entry = build_wikidata_author_entry(author_response_json, line, wikidata_author_id)
+            writer.writerow(entry)
+
+
 # Main mapping routine
 
 def map_records(inputfile, outputfile):
+
     print("Mapping", len(inputfile), "records in", outputfile)
     with open(outputfile, 'w') as csvfile:
-        fieldnames = ['gnd',
-                      'wikidata',
-                      'onb',
-                      'name',
-                      'genre',
-                      'occupation',
-                      'freebase',
-                      'viaf',
-                      'bnf',
-                      'nkc',
-                      'nta',
-                      'imslp',
-                      'dbpedia',
-                      'music_brainz_artist_id']
-        writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=fieldnames, lineterminator='\n')
+        writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=wikidata_author_fieldnames, lineterminator='\n')
         writer.writeheader()
-
-        # GND cache contains aggregated GND IDs
-        gnd_cache = []
-
-        f = codecs.open(inputfile, 'r')
-        for idx, line in enumerate(f):
-            print repr(line)
-            if idx > 0:
-                row = line.split(";")
-                gnd = row[GND_COL].split('/')[-1]
-                if gnd not in gnd_cache:
-                    # query Wikidata by GND ID, where GND Identifier is a property P227
-                    # e.g. https://wdq.wmflabs.org/api?q=string[227:118576291] for Gustav Mahler
-                    query = WIKIDATA_API_URL + 'string[227:' + gnd + ']'
-                    print 'query:', query
-                    r = process_http_query(query)
-                    #r = requests.get(query)
-                    #if(r.status_code != 200):
-                    #    print("Request error:", r.url)
-
-                    # extract wikidata author ID
-                    print 'response content:', r.content
-                    wikidata_author_id = r.json()[ITEMS_JSON][0]
-                    print 'wikidata_author_id', wikidata_author_id
-                    if wikidata_author_id:
-                        gnd_cache.append(wikidata_author_id)
-                        # query Wikidata by wikidata ID for an author
-                        # e.g. https://www.wikidata.org/wiki/Q7304 for Gustav Mahler
-                        # https://wdq.wmflabs.org/api?q=items[7304]&props=106,136
-                        #query = 'https://www.wikidata.org/wiki/Q' + str(wikidata_author_id) + ']'
-                        query_author = WIKIDATA_API_URL + ITEMS_JSON + '[' + str(wikidata_author_id) + ']&' + \
-                                       PROPS_JSON + '=' + ", ".join(str(e) for e in properties)
-                        print 'query author:', query_author
-                        author_response_json = process_http_query(query_author)
-                        print 'response wikidata content:', author_response_json.content
-                        # store author wikidata JSON in file onb_id_wikidata_id.json
-                        #genres = author_response_json.json()[repr(GENRE_PROP)]
-                        #for property in author_response_json.json()["props"]:
-                        #    print 'property', property
-                        #    genre = property[repr(GENRE_PROP)]
-                        #genres_list = author_response_json.json()[PROPS_JSON][str(GENRE_PROP)]
-                        #genres = " ".join(str(genre[2]) for genre in genres_list)
-                        #print 'genres', genres
-                        #json_data = author_response_json.json()
-                        #json_data_str = author_response_json.content.replace("[],","")
-                        #json_data = json.loads(json_data_str)
-                        print 'json_data:', author_response_json
-                        genres = extract_property_value(author_response_json, GENRE_PROP)
-                        occupations = extract_property_value(author_response_json, OCCUPATION_PROP)
-                        freebase = extract_property_value(author_response_json, FREEBASE_ID_PROP)
-                        viaf = extract_property_value(author_response_json, VIAF_ID_PROP)
-                        bnf = extract_property_value(author_response_json, BNF_ID_PROP)
-                        nkc = extract_property_value(author_response_json, NKC_ID_PROP)
-                        nta = extract_property_value(author_response_json, NTA_ID_PROP)
-                        imslp = extract_property_value(author_response_json, IMSLP_ID_PROP)
-                        music_brainz_artist = extract_property_value(author_response_json, MUSIC_BRAINZ_ARTIST_ID_PROP)
-
-                        values = [
-                            row[GND_COL]
-                            , wikidata_author_id
-                            , row[ONB_COL]
-                            , row[NAME_COL]
-                            , genres
-                            , occupations
-                            , freebase
-                            , viaf
-                            , bnf
-                            , nkc
-                            , nta
-                            , imslp
-                            , ''
-                            , music_brainz_artist
-                        ]
-
-                        entry = dict(zip(fieldnames, values))
-
-                        writer.writerow(entry)
+        store_author_data_by_gnd(inputfile, writer)
 
 
 # Command line parsing
