7981ebbeeae4affe43458875451549ed98033e4d
rgraf <roman.graf@ait.ac.at>
2017-06-16 16:01:02 +0200

    #3 Uploaded Europeana coins in JSON format.

--- europeanaJsonLoader.py
+++ europeanaJsonLoader.py
@@ -11,9 +11,11 @@ import glob
 
 
 INPUT_FILE = 'agents_in_europeana.txt'
+COINS_INPUT_FILE = 'coins_in_europeana.txt'
 JSON_EXT = '.json'
 OUTPUT_DIR = 'output'
 OUTPUT_DIR_2 = 'output2'
+OUTPUT_DIR_COINS = 'output-coins'
 SLASH = '/'
 
 
@@ -51,6 +53,24 @@ def save_json(outputdir, filename, data):
         response = json.loads(data)
         # print('Loading agent url:', agent, ', count:', count)
         write_json_file(outputdir, filename, response)
+
+    except Exception as ex:
+        print('Error loading JSON file:', filename, 'content:', data)
+        print(ex)
+        res = False
+
+    return res
+
+
+def save_json_list(outputdir, filename, data):
+
+    res = True
+
+    try:
+        response = json.loads(data)
+        items = response['items']
+        for item in items:
+            write_json_file(outputdir, filename.replace(JSON_EXT, item['id'].replace('/','-') + JSON_EXT), item)
 
     except Exception as ex:
         print('Error loading JSON file:', filename, 'content:', data)
@@ -107,5 +127,43 @@ def unitTest():
     print('Loading of Europeana agents completed.')
 
 
-unitTest()
-    
+def loadCoinsTest():
+
+    print('Loading of Europeana coins started ...')
+
+    coins = []
+    COIN_ROWS = 100
+
+    # read coins links from input file
+    with open(COINS_INPUT_FILE) as f:
+        coin = f.readlines()
+        # remove whitespace characters
+        coins = [x.strip() for x in coin]
+
+    count = 0
+    for coin in coins:
+        location_id = None
+        url_address = None
+        try:
+            url_address = "http://www.europeana.eu/api/v2/search.json?wskey=api2demo&profile=rich&rows=" + str(COIN_ROWS) + "&query=%22/" + \
+                          coin.replace("http://data.europeana.eu/place/base/", "place/base/") + "%22%20AND%20coin"
+            location_id = coin.split(SLASH)[-1]
+            filename = str(location_id) + JSON_EXT
+
+            if not is_stored_as_json_file(OUTPUT_DIR_COINS + SLASH + filename):
+                content = urllib.urlopen(url_address).read()
+                if not save_json_list(OUTPUT_DIR_COINS, filename, content):
+                    save_text(OUTPUT_DIR_COINS, filename, content)
+
+        except Exception as ex:
+            print('Error loading coin:', coin, 'id:', location_id, 'url:', url_address, 'count:', count)
+            print(ex)
+
+        count = count + 1
+
+    assert(len(coins) > 0)
+    print('Loading of Europeana coins completed.')
+
+
+#unitTest()
+loadCoinsTest()
